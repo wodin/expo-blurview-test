f6e527f4ec211addcb405ef1049dc37883ee9dab
Michael Wood <esiotrot@gmail.com>
2021-08-28 14:38:07 +0200

    Install @react-native-community/blur and use it

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import 'expo-dev-client';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "expo-dev-client";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { BlurView } from "@react-native-community/blur";
 
 export default function App() {
+  const uri = "https://github.com/Kureev/react-native-blur/raw/v3.6.1/example/bgimage.jpeg";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image key={"blurryImage"} source={{ uri }} style={styles.absolute} />
+      <Text style={styles.absolute}>Hi, I am some blurred text</Text>
+      {/* in terms of positioning and zIndex-ing everything before the BlurView will be blurred */}
+      <BlurView
+        style={styles.absolute}
+        blurType="light"
+        blurAmount={10}
+        reducedTransparencyFallbackColor="white"
+      />
+      <Text>
+        I'm the non blurred text because I got rendered on top of the BlurView
+      </Text>
     </View>
   );
 }
@@ -15,8 +28,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  absolute: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
